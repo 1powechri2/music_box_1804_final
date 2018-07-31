e6a8661bad44ecd8f7c3976ac3a94fa34f593f67
Chris Powell <chris.ian.powell@gmail.com>
2018-07-31 09:51:14 -0600

    admin functions hangin me up

---
+++ spec/features/admin_can_create_new_genre_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Admnin visits genre index' do
+  it 'can create a new genre' do
+    genre_1 = Genre.create(name: "Rock")
+    genre_2 = Genre.create(name: "Blues")
+    genre_3 = Genre.create(name: "Japanese Noise Music")
+    admin = User.create(username: 'dood', password: '786afd', role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit genres_path
+
+    click_on("Create a New Genre")
+
+    fill_in :genre_name, with: 'Grind Core'
+
+    click_on("Create Genre" )
+
+    expect(current_path).to be(genres_path)
+    expect(page).to have_content('Grind Core')
+  end
+end
